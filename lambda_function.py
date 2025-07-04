f21ea79dcc35332170e0aae5c96c1c0b9fafc620
Kelvin Jaison <82458499+KelvinJais@users.noreply.github.com>
2025-07-04 16:58:38 -0400

    cleaning up

--- lambda_function.py
+++ lambda_function.py
@@ -25,7 +25,27 @@ def upload_s3_folder():
             file_path = os.path.join(root, file)
             s3_path = os.path.join(prefix, os.path.relpath(file_path, '/tmp/data'))
             bucket.upload_file(file_path, s3_path)
-        
+
+def download_creds():
+    s3 = boto3.client('s3')
+    bucket_name = 'allseeings3data'        # Replace with your bucket name
+    object_key = 'creds.py'                 # Replace with the S3 key if it's in a subfolder, e.g., 'folder/creds.py'
+    local_filename = 'creds.py'             # Local path to save the file
+    # Download the file
+    s3.download_file(bucket_name, object_key, local_filename) 
+
+def delete_creds():
+    file_path = "creds.py"
+    try:
+        os.remove(file_path)
+        print(f"File '{file_path}' has been deleted successfully.")
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+    except PermissionError:
+        print(f"Permission denied to delete the file '{file_path}'.")
+    except Exception as e:
+        print(f"Error occurred while deleting the file: {e}")
+
 def lambda_handler(event, context):
     download_s3_folder()
     main()
