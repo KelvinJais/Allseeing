e603e26386b5a5695b8f8eabbb5f2c2fb09b9dd8
Kelvin Jaison <82458499+KelvinJais@users.noreply.github.com>
2025-08-17 13:50:12 -0400

    fix nutanix and adding google

--- companies/google.py
+++ companies/google.py
@@ -7,22 +7,31 @@ import asyncio
 import aiohttp
 
 async def extractor():
-    url="https://careers.nutanix.com/en/jobs/?search=Software+Engineer&country=United+States&pagesize=20#results"
-    headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:139.0) Gecko/20100101 Firefox/139.0"}
+    url = "https://www.google.com/about/careers/applications/jobs/results/?q=%22Software%20Engineer%22&location=United%20States&target_level=MID&target_level=EARLY"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:139.0) Gecko/20100101 Firefox/139.0"
+    }
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as response:
-            data=await response.text()
-            html=HTMLParser(data)
-            search_results=html.css("div.card-body")
-            items={}
-            detected_time=datetime.now(timezone.utc).isoformat(timespec='seconds').replace('+00:00','Z')
+            data = await response.text()
+            html = HTMLParser(data)
+            search_results = html.css("li.lLd3Je")  # each job posting
+            
+            items = {}
+            detected_time = datetime.now(timezone.utc).isoformat(timespec='seconds').replace('+00:00','Z')
             for search_result in search_results:
-                item={"title":search_result.css_first("h3").text(),
-                      "url":"https://careers.nutanix.com/"+search_result.css_first("a").attributes["href"],
-                    "jobId":search_result.css("li.list-inline-item")[2].text().strip().replace(" ", ""),
-                    "detected":detected_time
-                      }
-                items[item["jobId"]]=item
+                title_tag = search_result.css_first("h3.QJPWVe")
+                link_tag = search_result.css_first("a[aria-label^='Learn more about']")
+                job_id = search_result.attributes.get("jsdata", "")
+                
+                if title_tag and link_tag:
+                    item = {
+                        "title": title_tag.text(),
+                        "url": "https://www.google.com" + link_tag.attributes["href"],
+                        "jobId":"https://www.google.com" + link_tag.attributes["href"],
+                        "detected": detected_time
+                    }
+                    items[item["jobId"]] = item
             return items
         #print(search_result.css("li.list-inline-item")[2].text())
 def main(current_jobs,test=False):
