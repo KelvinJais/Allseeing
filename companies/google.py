7f8a6c913467c78bb8c684e1aec2c8c8c8ecc8fd
Kelvin Jaison <82458499+KelvinJais@users.noreply.github.com>
2025-08-18 10:55:08 -0400

    Adding Aggregation to google

--- companies/google.py
+++ companies/google.py
@@ -34,31 +34,31 @@ async def extractor():
                     items[item["jobId"]] = item
             return items
         #print(search_result.css("li.list-inline-item")[2].text())
+#Aggregate
 def main(current_jobs,test=False):
-    # Getting Company name from the file name
     company_name=os.path.basename(__file__)[:-3]
     file_path=os.path.join("/tmp","data",f"{company_name}_jobs_list.json")
-    #Initializing files
     if not os.path.exists(file_path):
-        job_data=current_jobs
+        job_data= current_jobs
         load_download.download_json(job_data,f"{company_name}_jobs_list")
         load_download.download_json(job_data,f"{company_name}_jobs_list_t_new_jobs")
     else:
         if test:
             new_job_data=load_download.load_json(f"{company_name}_jobs_list_t_new_jobs")
         else:
-            new_job_data=current_jobs
+            new_job_data= current_jobs
         old_job_data=load_download.load_json(f"{company_name}_jobs_list")
         brand_new_jobs=[]
         for job in new_job_data.keys():
             if job not in old_job_data:
                 brand_new_jobs.append(new_job_data[job])
+                old_job_data[job]=new_job_data[job]
         if not test:
-            load_download.download_json(new_job_data,f"{company_name}_jobs_list")
+            #load_download.download_json(new_job_data,f"{company_name}_jobs_list")
+            load_download.download_json(old_job_data,f"{company_name}_jobs_list")
         print(len(brand_new_jobs),"new jobs at",company_name)
         return brand_new_jobs
 
-
 if __name__ =="__main__":
     current_jobs=asyncio.run(extractor())
-    #main(current_jobs)
+    main(current_jobs)
